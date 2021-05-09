d1729153540a803ee910a410800f4e90a7dbbfbb
Maanas Nair <bee.birdiecorporation@gmail.com>
2021-05-09 11:02:20 +0530

    initial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//import css
+import './style.css';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//jSX Rules
+// return single element
+// use camelCase for <html></html>
+// className instead of class
+// close every element
+// formatting
+
+
+//setup vars
+const firstBook = {
+  img: 'https://images-eu.ssl-images-amazon.com/images/I/91m%2Bcb%2BijPL._AC_UL200_SR200,200_.jpg',
+  title: "Karma: A Yogi's Guide to Crafting Your Destiny",
+  author: 'Sadhguru'
+
+};
+const secondBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/81cpDaCJJCL._AC_UL200_SR200,200_.jpg',
+  title: "The Psychology of Money",
+  author: 'Morgan Housel'
+
+};
+
+
+
+function BookList(){
+  return (
+    <>
+    <section className="booklist">
+      <Book img={firstBook.img} title={firstBook.title} author={firstBook.author} />
+      <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} />
+    </section>
+    </>
+  );
+}
+
+const Book = ({img, title, author}) =>{
+  // const {img, title, author} = props
+  // console.log(props);
+  return <article className="book">
+    <img src={img} alt="First Book" />
+    <h1>{title}</h1>
+    <h4>{author}</h4>
+  </article>
+}
+// const Image = () =>{
+//   return <img src="https://images-eu.ssl-images-amazon.com/images/I/91m%2Bcb%2BijPL._AC_UL200_SR200,200_.jpg" alt="Book's Image" />
+// }
+// const Title = () =>{
+//   return <h1>Karma: A Yogi's Guide to Crafting Your Destiny</h1>
+// }
+
+// const Author = () =>{
+//   return <h4 style={{color:'#617d98',}}>Sadhguru</h4>
+// }
+
+// Nested components
+// const HelloWorld = () => <h1>hello wooorld</h1>
+// const Message = () => <p>noice</p>
+
+
+// const Greeting = () =>{
+//   return React.createElement('div', {}, React.createElement('h1', {}, 'hello world'))
+// }
+
+ReactDOM.render(<BookList/>, document.getElementById('root'))
