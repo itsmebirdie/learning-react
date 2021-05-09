daebefcb2b1a9196659724bee3c33194bafbcaa2
Maanas Nair <bee.birdiecorporation@gmail.com>
2021-05-09 11:36:06 +0530

    Simple Array List

--- src/index.js
+++ src/index.js
@@ -12,65 +12,42 @@ import './style.css';
 // formatting
 
 
-//setup vars
-const firstBook = {
-  img: 'https://images-eu.ssl-images-amazon.com/images/I/91m%2Bcb%2BijPL._AC_UL200_SR200,200_.jpg',
-  title: "Karma: A Yogi's Guide to Crafting Your Destiny",
-  author: 'Sadhguru'
-
-};
-const secondBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/81cpDaCJJCL._AC_UL200_SR200,200_.jpg',
-  title: "The Psychology of Money",
-  author: 'Morgan Housel'
-
-};
-
-
-
+//setup vars{
+const books = [
+  {
+    img: 'https://images-eu.ssl-images-amazon.com/images/I/91m%2Bcb%2BijPL._AC_UL200_SR200,200_.jpg',
+    title: "Karma: A Yogi's Guide to Crafting Your Destiny",
+    author: 'Sadhguru'
+    
+  },
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81cpDaCJJCL._AC_UL200_SR200,200_.jpg',
+    title: "The Psychology of Money",
+    author: 'Morgan Housel'
+    
+  },
+];
+const names = ['John', 'Peter', 'Susan'];
+const newNames = names.map((name)=> {
+  return <h1>{name} </h1>;
+});
+console.log(newNames)
+  
 function BookList(){
   return (
     <>
     <section className="booklist">
-      <Book img={firstBook.img} title={firstBook.title} author={firstBook.author} >
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum minus ullam facilis repudiandae molestiae aliquid nemo earum recusandae temporibus incidunt?</p>
-      </Book>
-      <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} />
+      {newNames}
     </section>
     </>
   );
 }
 
 const Book = (props) =>{
-  const { img, title, author, children} = props;
+  const { } = props;
   console.log(props)
-  // const {img, title, author} = props
-  // console.log(props);
   return <article className="book">
-    <img src={img} alt="First Book" />
-    <h1>{title}</h1>
-    <h4>{author}</h4>
-    {children}
   </article>
 }
-// const Image = () =>{
-//   return <img src="https://images-eu.ssl-images-amazon.com/images/I/91m%2Bcb%2BijPL._AC_UL200_SR200,200_.jpg" alt="Book's Image" />
-// }
-// const Title = () =>{
-//   return <h1>Karma: A Yogi's Guide to Crafting Your Destiny</h1>
-// }
-
-// const Author = () =>{
-//   return <h4 style={{color:'#617d98',}}>Sadhguru</h4>
-// }
-
-// Nested components
-// const HelloWorld = () => <h1>hello wooorld</h1>
-// const Message = () => <p>noice</p>
-
-
-// const Greeting = () =>{
-//   return React.createElement('div', {}, React.createElement('h1', {}, 'hello world'))
-// }
 
 ReactDOM.render(<BookList/>, document.getElementById('root'))
