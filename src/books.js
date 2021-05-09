609dae561ab22febb0c0a4c55b482e1220c1e6f0
Maanas Nair <bee.birdiecorporation@gmail.com>
2021-05-09 12:33:55 +0530

    Import and Export statements

---
+++ src/books.js
@@ -0,0 +1,23 @@
+export const books = [
+    { 
+      id:1,
+      img: 'https://images-eu.ssl-images-amazon.com/images/I/91m%2Bcb%2BijPL._AC_UL200_SR200,200_.jpg',
+      title: "Karma: A Yogi's Guide to Crafting Your Destiny",
+      author: 'Sadhguru'
+      
+    },
+    {
+      id:2,
+      img: 'https://images-na.ssl-images-amazon.com/images/I/81cpDaCJJCL._AC_UL200_SR200,200_.jpg',
+      title: "The Psychology of Money",
+      author: 'Morgan Housel'
+      
+    },
+    {
+      id:3,
+      img: 'https://images-eu.ssl-images-amazon.com/images/I/61r-WomIz0L._AC_UL200_SR200,200_.jpg',
+      title: 'One Arranged Murder',
+      author: 'Chetan Bhagat'
+    },
+  ];
+  
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 
 //import css
 import './style.css';
+//import js
+import {books} from './books';
+import Book from './Book';
+import {greeting} from './testing/testing';
 
 //jSX Rules
 // return single element
@@ -13,30 +17,9 @@ import './style.css';
 
 
 //setup vars{
-const books = [
-  { 
-    id:1,
-    img: 'https://images-eu.ssl-images-amazon.com/images/I/91m%2Bcb%2BijPL._AC_UL200_SR200,200_.jpg',
-    title: "Karma: A Yogi's Guide to Crafting Your Destiny",
-    author: 'Sadhguru'
-    
-  },
-  {
-    id:2,
-    img: 'https://images-na.ssl-images-amazon.com/images/I/81cpDaCJJCL._AC_UL200_SR200,200_.jpg',
-    title: "The Psychology of Money",
-    author: 'Morgan Housel'
-    
-  },
-  {
-    id:3,
-    img: 'https://images-eu.ssl-images-amazon.com/images/I/61r-WomIz0L._AC_UL200_SR200,200_.jpg',
-    title: 'One Arranged Murder',
-    author: 'Chetan Bhagat'
-  },
-];
   
 function BookList(){
+  console.log(greeting)
   return (
     <>
     <section className="booklist">
@@ -50,30 +33,7 @@ function BookList(){
   );
 }
 
-const Book = ({ img, title, author}) =>{
-  // attribute, eventHandler
-  // onClick, onMouseOver
 
-  const clickHandler = (e) => {
-    console.log(e);
-    console.log(e.target);
-    alert(title)
-  }
-  const complexExample = (author) => {
-    console.log(author)
-  }
-  return (
-    <article className="book" onMouseOver={() => {
-      alert(title)
-    }} >
-    <img src={img} alt="" />
-    <h1 onClick={() => console.log(title)}>{title}</h1>
-    <h4>{author} </h4>
-    <button type="button" onClick={clickHandler}>reference example</button>
-    <button type="button" onClick={() => complexExample(author)}>more complex example</button>
-  </article>
-  )
-}
 
 
 ReactDOM.render(<BookList/>, document.getElementById('root'))
